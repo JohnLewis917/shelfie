cba975031eefaa7bfeff0a37378a09631fe1c882
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-01 16:24:31 -0600

    initial

---
+++ src/Components/Product/Product.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+
+class Product extends Component {
+    constructor (){
+        super()
+    }
+    render(){
+        return (
+            <h1>
+                
+                
+                
+            </h1>
+        )
+    }
+}
+    
+
+export default Product;
