0371aa61750d97269b492952a119ea3500c456e6
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-01 16:24:02 -0600

    initial

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import Product from '../Product/Product';
+
+
+class Dashboard extends Component {
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <div>
+                <Product/>
+                <h1>
+                    
+                    
+                </h1>
+            </div>
+        )
+    }
+}
+    
+        
+    
+    
+
+
+         
+export default Dashboard;
