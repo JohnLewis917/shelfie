f94b9c33c4b7c98d0fb2be792b189ccfe6665dcb
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-01 20:03:18 -0600

    first changes 11-1

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,23 +1,40 @@
 import React, {Component} from 'react';
 
 class Form extends Component {
-    constructor() {
-        super(
+    constructor(props) {
+        super(props)
             this.state = {
                 product_name: '',
-                price: 0,
+                price: '',
                 img: ''
-              }
-        )
+            }
+        // this.handleSubmit = this.handleSubmit.bind(this)
+        
+    }
+    handleChange(event){
+        this.setState({product_name: event.target.product_name})
+    }
+    handleChange(event){
+        this.setState({price: event.target.price})
+    }
+    handleChange(event){
+        this.setState({img: event.target.img})
     }
     render() {
         return (
             <div>
-                <input className="input-1" type="text"></input>
-                <input className="input-2" type="text"></input>
-                <input className="input-3" type="text"></input>
-                <button classname="button-1" type="text">Cancel</button>
-                <button className="button-2" type="text">Add to Inventory</button>
+                <form onSubmit={this.handleSubmit}>
+                    <input className="input-1" type="text" value={this.state.product_name} onChange={this.handleChange} placeholder="Product Name"/>
+                    <br></br>
+                    <input className="input-2" type="text" value={this.state.price} onChange={this.handleChange} placeholder="Price"/>
+                    <br></br>
+                    <input className="input-3" type="text" value={this.state.img} onChange={this.handleChange} placeholder="Image"/>
+                    <br></br>
+                    <br></br>
+
+                    <button className="button-1" type="text">Cancel</button>
+                    <button className="button-2" type="submit">Add to Inventory</button>
+                </form>
                 
             </div> 
         )
