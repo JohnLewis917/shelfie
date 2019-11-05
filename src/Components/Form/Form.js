ee74bdb16f83e48904d4daa6c2dbab059435cef5
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-04 20:43:22 -0700

    changes 11-4

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -9,30 +9,42 @@ class Form extends Component {
                 img: ''
             }
         // this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleName= this.handleName.bind(this)
+        this.handlePrice= this.handlePrice.bind(this)
+        this.handleImg= this.handleImg.bind(this)
+        
         
     }
-    handleChange(event){
+    handleName(event){
+        console.log(event)
         this.setState({product_name: event.target.product_name})
     }
-    handleChange(event){
+    handlePrice(event){
         this.setState({price: event.target.price})
     }
-    handleChange(event){
+    handleImg(event){
         this.setState({img: event.target.img})
+    }
+    buttonClick() {
+        this.setState ={
+        product_name: '',
+        price: '',
+        img: ''
+        }
     }
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input className="input-1" type="text" value={this.state.product_name} onChange={this.handleChange} placeholder="Product Name"/>
+                <form>
+                    <input className="input-1" type="text"  onChange={this.handleChange} placeholder="Product Name"/>
                     <br></br>
-                    <input className="input-2" type="text" value={this.state.price} onChange={this.handleChange} placeholder="Price"/>
+                    <input className="input-2" type="text"  onChange={this.handleChange} placeholder="Price"/>
                     <br></br>
-                    <input className="input-3" type="text" value={this.state.img} onChange={this.handleChange} placeholder="image"/>
+                    <input className="input-3" type="text"  onChange={this.handleChange} placeholder="image"/>
                     <br></br>
                     <br></br>
 
-                    <button className="button-1" type="text">Cancel</button>
+                    <button className="button-1" type="reset" onClick={() => this.buttonClick()}>Cancel</button>
                     <button className="button-2" type="submit">Add to Inventory</button>
                 </form>
                 
