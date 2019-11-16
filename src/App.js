1095a9af22298b5e78c5d49c6a1d7db76a67ed5c
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-16 16:58:06 -0700

    changes 11-16

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
 import React, { Component } from "react";
-// import {Component} from 'react';
-import axios from "axios";
 import "./App.css";
-import Dashboard from "./Components/Dashboard/Dashboard";
-import Form from "./Components/Form/Form";
 import Header from "./Components/Header/Header";
+import {Switch, Route} from 'react-router-dom'
+import Dashboard from './Components/Dashboard/Dashboard'
+import Form from './Components/Form/Form'
+
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      inventory: []
-    };
+    
   }
-  componentDidMount(){
-    this.getInventory()
-  }
-  getInventory(){
-    axios.get('/api/App')
-    .then(res => {
-        console.log(res.data)
-        this.setState({
-            inventory: res.data
-        })
-        console.log(res.data)
-        
-    })
- }
+  
   render() {
     return (
       <div className="App">
         <Header />
+        <Switch>
+            <Route exact path='/' component={Dashboard}/>
+            <Route path='/Form' component={Form}/>
+         </Switch>
         
-          <Dashboard inventory={this.state.inventory}/>
-    
-        <Form />
       </div>
     );
   }
