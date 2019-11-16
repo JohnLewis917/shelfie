7cc561276e57722c118145af74113fc0e91d5692
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-15 20:49:45 -0700

    changes 11-15

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,20 +1,18 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
 
 class Product extends Component {
-    constructor (){
-        super()
-    }
-    render(){
-        return (
-            <h1>
-                
-                
-                
-            </h1>
-        )
-    }
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <h6>{this.props.inventoryObj.product_name}</h6>
+        <h6>{this.props.inventoryObj.price}</h6>
+        <img src={this.props.inventoryObj.img} height="150px" alt="" />
+      </div>
+    );
+  }
 }
-    
 
 export default Product;
