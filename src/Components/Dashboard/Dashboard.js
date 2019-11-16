4d4d6d59401c1f82b4008855557e464358b8d0a5
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-15 20:50:16 -0700

    changes 11-15

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,29 +1,20 @@
-import React, {Component} from 'react';
-import Product from '../Product/Product';
-
+import React, { Component } from "react";
+import Product from "../Product/Product";
 
 class Dashboard extends Component {
-    constructor(){
-        super()
-    }
-    
-    render(){
-        return(
-            <div>
-                <Product/>
-                <h1>
-                    
-                    
-                </h1>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.inventory.map(el => (
+          <Product key={el.id} inventoryObj={el} />
+        ))}
+      </div>
+    );
+  }
 }
-    
-        
-    
-    
 
-
-         
 export default Dashboard;
