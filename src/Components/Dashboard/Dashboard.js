48a9a3c5ffa645afd5d3ccd3dc3502b68f210b41
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-16 16:57:39 -0700

    changes 11-16

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,17 +1,39 @@
 import React, { Component } from "react";
 import Product from "../Product/Product";
+import Form from '../Form/Form'
+import axios from "axios";
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      inventory: []
+    };
   }
+  componentDidMount(){
+    this.getInventory()
+  }
+  getInventory(){
+    axios.get('/api/Dashboard')
+    .then(res => {
+        console.log(res.data)
+        this.setState({
+            inventory: res.data
+        })
+        console.log(res.data)
+        
+    })
+ }
 
   render() {
     return (
       <div>
-        {this.props.inventory.map(el => (
+        {this.state.inventory.map(el => (
           <Product key={el.id} inventoryObj={el} />
         ))}
+        
+        <Form/>
+        
       </div>
     );
   }
