b785a50edb790af7dee47378c07c6691186b405c
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-16 16:57:27 -0700

    changes 11-16

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import axios from 'axios'
+
 
 class Form extends Component {
     constructor(props) {
@@ -18,22 +20,28 @@ class Form extends Component {
     }
     handleName(event){
         console.log(event)
-        this.setState({product_name: event.target.product_name})
+        this.setState({product_name: event.target})
     }
     handlePrice(event){
-        this.setState({price: event.target.price})
+        this.setState({price: event.target})
     }
     handleImg(event){
-        this.setState({img: event.target.img})
+        console.log(event)
+        this.setState({img: event.target})
     }
     buttonClick() {
-        
         this.setState ={
         product_name: '',
         price: 0,
-        img: ''
+        img: 'https://timedotcom.files.wordpress.com/2018/11/sweetfoam-sustainable-product.jpg?quality=85'
         }
         
+    }
+    addInventory(){
+        axios.post('/api/Dashboard', this.props)
+        .then(res => {
+        this.props.history.push('/')
+        })
     }
     render() {
         
@@ -42,16 +50,17 @@ class Form extends Component {
                 <form id="form1">
                     <img src={this.state.img} height="150px" alt=""/>
                     <br></br>
-                    <input className="input-3" type="text"  size="35" onChange={this.handleChange} placeholder="imageURL"/>
+                    <input className="input-3" type="text"   size="35" onChange={this.handleImg} placeholder="imageURL"/>
                     <br></br>
-                    <input className="input-1" type="text"  size="35" onChange={this.handleChange} placeholder="Product Name"/>
+                    <input className="input-1" type="text"   size="35"  onChange={this.handleName} placeholder="Product Name"/>
                     <br></br>
-                    <input className="input-2" type="text"  size="35" onChange={this.handleChange} placeholder="Price"/>
+                    <input className="input-2" type="text"   size="35"  onChange={this.handlePrice} placeholder="Price"/>
                     <br></br>
                     <br></br>
 
                     <button className="button-1" type="reset" onClick={() => this.buttonClick()}>Cancel</button>
-                    <button className="button-2" type="submit" form="form1">Add to Inventory</button>
+                    <button className="button-2" type="submit" form="form1" onClick={this.addInventory}>Add to Inventory</button>
+                    
                 </form>
                 
             </div> 
