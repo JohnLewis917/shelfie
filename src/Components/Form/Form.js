176fa9cc1753d533f68b300b7e5e53f4b159fa16
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-18 17:32:34 -0700

    changes 11-18

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios'
+import {withRouter} from 'react-router-dom'
 
 
 class Form extends Component {
@@ -11,34 +12,43 @@ class Form extends Component {
                 price: '',
                 img: 'https://timedotcom.files.wordpress.com/2018/11/sweetfoam-sustainable-product.jpg?quality=85'
             }
-        // this.handleSubmit = this.handleSubmit.bind(this)
+        
         this.handleName= this.handleName.bind(this)
         this.handlePrice= this.handlePrice.bind(this)
         this.handleImg= this.handleImg.bind(this)
+        this.addInventory=this.addInventory.bind(this)
         
         
+    }
+    componentDidMount(){
+        const {product_name, price, img} = this.props.currentObj
+        this.setState({
+            product_name: product_name,
+            price: price,
+            img: img
+        })
     }
     handleName(event){
-        console.log(event)
-        this.setState({product_name: event.target})
+        this.setState({product_name: event.target.value})
+        
     }
     handlePrice(event){
-        this.setState({price: event.target})
+        this.setState({price: event.target.value})
     }
     handleImg(event){
-        console.log(event)
-        this.setState({img: event.target})
+        this.setState({img: event.target.value})
     }
     buttonClick() {
-        this.setState ={
+        this.setState ({
         product_name: '',
         price: 0,
         img: 'https://timedotcom.files.wordpress.com/2018/11/sweetfoam-sustainable-product.jpg?quality=85'
-        }
+        })
         
     }
+    
     addInventory(){
-        axios.post('/api/Dashboard', this.props)
+        axios.post('/api/Dashboard', this.state)
         .then(res => {
         this.props.history.push('/')
         })
@@ -47,19 +57,19 @@ class Form extends Component {
         
         return (
             <div>
-                <form id="form1">
+                <form onSubmit={this.addInventory}>
                     <img src={this.state.img} height="150px" alt=""/>
                     <br></br>
-                    <input className="input-3" type="text"   size="35" onChange={this.handleImg} placeholder="imageURL"/>
+                    <input className="input-3" type="text"  value={this.state.img} size="35" onChange={this.handleImg} placeholder="imageURL"/>
                     <br></br>
-                    <input className="input-1" type="text"   size="35"  onChange={this.handleName} placeholder="Product Name"/>
+                    <input className="input-1" type="text"   value={this.state.product_name} size="35"   onChange={this.handleName} placeholder="Product Name"/>
                     <br></br>
-                    <input className="input-2" type="text"   size="35"  onChange={this.handlePrice} placeholder="Price"/>
+                    <input className="input-2" type="text"   value={this.state.price} size="35"  onChange={this.handlePrice} placeholder="Price"/>
                     <br></br>
                     <br></br>
 
                     <button className="button-1" type="reset" onClick={() => this.buttonClick()}>Cancel</button>
-                    <button className="button-2" type="submit" form="form1" onClick={this.addInventory}>Add to Inventory</button>
+                    <button className="button-2" type="submit">Add to Inventory</button>
                     
                 </form>
                 
@@ -68,4 +78,4 @@ class Form extends Component {
     }
 }
 
-export default Form;
+export default withRouter(Form);
