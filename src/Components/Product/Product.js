ef672e01396b3b1c39d0dc1d90de27d88b539903
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-18 20:38:54 -0700

    new changes 11-18

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,21 +1,28 @@
 import React, { Component } from "react";
+import {withRouter} from 'react-router-dom'
 
 class Product extends Component {
   constructor() {
     super();
+    this.handleEdit = this.handleEdit.bind(this)
+  }
+
+  handleEdit(){
+    const {id} = this.props 
+    this.props.history.push(`/Edit/${id}`)
   }
   render() {
-    console.log(this.props)
+    // console.log(this.props)
     return (
       <div className = "product-box">
         <h6>{this.props.inventoryObj.product_name}</h6>
         <h6>{this.props.inventoryObj.price}</h6>
         <img src={this.props.inventoryObj.img} height="150px" alt="" />
-        <button className="delete" type="submit" onClick={() => this.props.deleteFn(this.props.inventoryObj.id)}>Delete</button>
-        <button className="edit" type="submit" >Edit</button>
+        <button className="delete"  onClick={() => this.props.deleteFn(this.props.inventoryObj.id)}>Delete</button>
+        <button className="edit"  onClick={this.handleEdit}>Edit</button>
       </div>
     );
   }
 }
 
-export default Product;
+export default withRouter(Product)
