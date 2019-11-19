b74aa1ecac20726488af71c0c551358c307f3425
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-18 20:39:08 -0700

    new changes 11-18

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+
 
 class Header extends Component {
-    constructor() {
-        super()
-    }
+    
+    
+    
     render(){
         return(
             <div>
                 <h1>
                     Shelfie
                 </h1>
+                <Link to='/'><button>Dashboard</button></Link>
+                <Link to='/Form'><button>Add Inventory</button></Link>
             </div>
         )
     }
