88a779c4e5ca1895510fbf27d05e552b5d8fe104
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-18 17:32:18 -0700

    changes 11-18

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -5,11 +5,14 @@ class Product extends Component {
     super();
   }
   render() {
+    console.log(this.props)
     return (
       <div className = "product-box">
         <h6>{this.props.inventoryObj.product_name}</h6>
         <h6>{this.props.inventoryObj.price}</h6>
         <img src={this.props.inventoryObj.img} height="150px" alt="" />
+        <button className="delete" type="submit" onClick={() => this.props.deleteFn(this.props.inventoryObj.id)}>Delete</button>
+        <button className="edit" type="submit" >Edit</button>
       </div>
     );
   }
